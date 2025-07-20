e95f8c10c8177789c9ce3fd82b4ea68492860b7f
Swend <politra59@bk.ru>
2025-07-20 04:02:50 +0300

    DecisionCell visual change

--- Assets/Scripts/Rooms/DecisionCell.cs
+++ Assets/Scripts/Rooms/DecisionCell.cs
@@ -9,14 +9,18 @@ public class DecisionCell : MonoBehaviour, IPointerClickHandler
     public event Action<DecisionCell> OnCellClick;
     public Vector2 Coordinates;
     public List<Vector2> WallsDirections = new List<Vector2>();
+    [SerializeField] Transform imagePrefab;
+    float spacing;
     public void Init(Vector2 coord, float spacing = 1f)
     {
+        this.spacing = spacing;
         Coordinates = coord;
         transform.localPosition = coord * spacing;
     }
 
     public void AddWallDirection(Vector2 dir)
     {
+        Instantiate(imagePrefab, transform.position + (Vector3)(dir * spacing), Quaternion.identity, transform);
         WallsDirections.Add(dir);
     }
 
