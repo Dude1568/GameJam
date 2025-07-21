57c701dc0154060beb611faa8e7c8889ef5ac7fc
Farid <farid.teresniak@hotmail.de>
2025-07-21 02:16:54 +0200

    Added footsteps and expand button sounds.

--- Assets/Scripts/UI/GoldTracker.cs
+++ Assets/Scripts/UI/GoldTracker.cs
@@ -5,8 +5,9 @@ public static class GoldTracker
     public static int gold = 0;
     private static TextMeshProUGUI tracker;
 
-    public static void Initialize(TextMeshProUGUI trackerText)
+    public static void Initialize(TextMeshProUGUI trackerText,int startingGold)
     {
+        gold = startingGold;
         tracker = trackerText;
         UpdateDisplay();
     }
