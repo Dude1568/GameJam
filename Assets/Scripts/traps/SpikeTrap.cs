c3ce7bd421d0bcf1b5943fb63b3aa544caac46d3
Farid <farid.teresniak@hotmail.de>
2025-07-21 03:39:44 +0200

    spiketrap modified

--- Assets/Scripts/traps/SpikeTrap.cs
+++ Assets/Scripts/traps/SpikeTrap.cs
@@ -1,25 +1,76 @@
+using System.Collections;
 using UnityEngine;
 
 public class SpikeTrap : Trap
 {
     public int damage = 10;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    [SerializeField] Sprite normalSprite;
+    [SerializeField] Sprite triggeredSprite;
+    [SerializeField] AudioClip triggeredClip;
+    [SerializeField] float clipVolume = 100f;
+    [SerializeField] float retriggerDuration = 1f;
+    [SerializeField] ParticleSystem triggeredPS;
+    SpriteRenderer spriteRenderer;
+    bool gotTriggered;
+    bool isFinished;
+    private void Awake()
     {
-        if (other.CompareTag("Enemy"))
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    //private void OnTriggerEnter2D(Collider2D other)
+    //{
+    //    if (!gotTriggered && other.CompareTag("Enemy"))
+    //    {
+    //        Trigger(other.gameObject);
+    //    }
+    //}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!gotTriggered && collision.CompareTag("Enemy"))
         {
-            Trigger(other.gameObject);
+            if (isFinished) { return; }
+            Trigger(collision.gameObject);
         }
     }
 
     public override void Trigger(GameObject target)
     {   
-        var health = target.GetComponent<EnemyHealth>();
-        if (health != null)
+        if(target.TryGetComponent(out EnemyHealth health))
         {
             health.TakeDamage(damage);
         }
 
+        StartCoroutine(TriggerRoutine());
         Use(); // reduce usage count
+    }
+
+    IEnumerator TriggerRoutine()
+    {
+        gotTriggered = true;
+        spriteRenderer.sprite = triggeredSprite;
+        if(Soundmanager.instance != null)
+            Soundmanager.instance.PlaySoundEffect(triggeredClip, transform, clipVolume);
+        if(triggeredPS != null)
+            Instantiate(triggeredPS,transform.position, Quaternion.identity);   
+
+        yield return new WaitForSeconds(retriggerDuration);
+        spriteRenderer.sprite = normalSprite;
+        gotTriggered = false;
+    }
+
+    protected override void OnEmpty()
+    {
+        StartCoroutine(DestroyRoutine());
+    }
+
+    IEnumerator DestroyRoutine()
+    {
+        isFinished = true;
+        yield return new WaitForSeconds(retriggerDuration + 1f);
+        Destroy(gameObject);
     }
 }
